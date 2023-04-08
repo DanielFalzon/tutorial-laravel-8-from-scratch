afcf27a465f1854e2f735a9aa65dd687ef63033f
daniel.falzon <daniel.falzon@raketech.com>
2023-04-08 17:23:41 +0200

    Lesson 27: Seeders and PHPDoc helper

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,14 @@
 <x-layout>
     <article>
         <h1>{!! $post->title !!}</h1>
-        <div>{!! $post->body !!}</div>
         <div>
-            <a href="/post/{{$post->slug}}">
+            Written By <a href="#">{{ $post->user->name }}</a> in <a href="/post/{{$post->slug}}">
                 <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
             </a>
+
         </div>
+        <br/>
+        <div><p>{!! $post->body !!}</p></div>
     </article>
 
     <a href="/">back home</a>
