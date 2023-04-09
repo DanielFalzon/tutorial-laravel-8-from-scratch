b23d14b5d324b6941aea162c495ae902e83d9964
daniel.falzon <danie.falzon6@gmail.com>
2023-04-10 00:18:04 +0200

    removed commented code

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -46,13 +46,6 @@
                 </div>
             </div>
 
-            {{--            <label>--}}
-            {{--                <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">--}}
-            {{--
-            {{--                </select>--}}
-            {{--            </label>--}}
-
-
         </div>
 
         <!-- Other Filters -->
