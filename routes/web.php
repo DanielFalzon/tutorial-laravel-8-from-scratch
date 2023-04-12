be1f8e25076abfab323a7d48c6b1c925512a22c9
daniel.falzon <danie.falzon6@gmail.com>
2023-04-12 23:16:12 +0200

    Lesson 45: Created user registration form

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 //route model binding
 //1. Wildcard name must match the model name + variable name
 Route::get('/post/{post}', [PostController::class, 'show']); //->whereAlpha, ->whereNumeric, options to add constraints to wildcard
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
