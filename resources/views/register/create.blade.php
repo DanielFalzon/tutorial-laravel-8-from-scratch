8310ac52e35658cf4b05ad4cd868b20fe3dbe6b0
daniel.falzon <danie.falzon6@gmail.com>
2023-04-12 23:34:25 +0200

    Lesson 47: Validation failing and old input

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -14,8 +14,13 @@
                                type="text"
                                name="name"
                                id="name"
+                               value="{{old('name')}}"
                                required
                         >
+
+                        @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
                     </div>
 
                     <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
@@ -25,19 +30,27 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{old('username')}}"
                            required
                     >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Email
                     </label>
                     <input class="border border-gray-400 p-2 w-full rounded"
-                        type="email"
-                        name="email"
-                        id="email"
-                        required
+                           type="email"
+                           name="email"
+                           value="{{old('email')}}"
+                           id="email"
+                           required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
@@ -49,13 +62,15 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <button type="submit"
-                    class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
+                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                    Submit
                 </button>
-
             </form>
         </main>
     </section>
