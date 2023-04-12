a86b08cd0859717d5212cd9417185ab1b6452a9f
daniel.falzon <danie.falzon6@gmail.com>
2023-04-12 23:59:12 +0200

    Lesson 49: Login and logout

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 //route model binding
 //1. Wildcard name must match the model name + variable name
 Route::get('/post/{post}', [PostController::class, 'show']); //->whereAlpha, ->whereNumeric, options to add constraints to wildcard
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy']);
